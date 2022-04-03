ac7272dbbb5a76d2fdd1b168a6e0e7436bf2a86f
Bible-min <102918831+Bible-min@users.noreply.github.com>
2022-04-04 02:06:08 +0900

    Update ReportSumHighlightsFree.jsx

--- src/components/ReportSumHighlightsFree.jsx
+++ src/components/ReportSumHighlightsFree.jsx
@@ -14,8 +14,8 @@ const ReportSumHighlightsFree = (props) => {
     eulguDangerList: [],
     kapguDangerList: [],
   })
-  
-  const [simpleData, simpleSetData] = useState({
+
+  const [simpledata, setSimpleData] = useState({
     complexType: "집합건물",
     isGuaranteeInsurance: true,
     isRepaymentSubject: true
@@ -25,51 +25,61 @@ const ReportSumHighlightsFree = (props) => {
 
   useEffect(() => {
     setData(props.data)
+    setSimpleData(props.simpledata)
   }, [props])
-
-  
-
 
   return (
     <div className={styles.summaryBox}>
       <div className={styles.summaryTitle} style={{marginBottom: '15px'}}>주요사항 요약</div>
-      {data &&
+      {simpledata &&
         <div>
-          {simpleData.isRepaymentSubject &&
+          {simpledata.isRepaymentSubject &&
             <>
               <Check className={styles.summaryBang}/>
+              <div className={styles.summarySpan} style={{
+                height:'10px'
+              }}></div>
               <div className={styles.summaryDiv} style={{
                 height:'35px'
               }}>최우선 변제가 가능할 수도 있어요!</div>
               <div className={styles.summaryBlock}></div>
             </>
           }
-          {!simpleData.isRepaymentSubject &&
+          {!simpledata.isRepaymentSubject &&
             <>
               <Check className={styles.summaryBang}/>
+              <div className={styles.summarySpan} style={{
+                height:'10px'
+              }}></div>
               <div className={styles.summaryDiv} style={{
                 height:'35px'
               }}>최우선 변제가 안되는 집이에요!</div>
               <div className={styles.summaryBlock}/>
             </>
           }
-          {simpleData.complexType === "집합건물" &&
+          {simpledata.complexType === "집합건물" &&
             <>
               <Check className={styles.summaryBang}/>
+              <div className={styles.summarySpan} style={{
+                height:'10px'
+              }}></div>
               <div className={styles.summaryDiv}>이 집은 '집합 건물'이에요!</div>
               <div className={styles.summaryBlock}/>
             </>
           }
-          {simpleData.complexType !== "집합건물" &&
+          {simpledata.complexType !== "집합건물" &&
             <>
               <Check className={styles.summaryBang}/>
+              <div className={styles.summarySpan} style={{
+                height:'10px'
+              }}></div>
               <div className={styles.summaryDiv}>이 집은 '일반 건물'이에요!</div>
               <div className={styles.summaryBlock}/>
             </>
           }
         </div>
       }
-      {!data &&
+      {!simpledata &&
         <>
           <div> loading... </div>
         </>
